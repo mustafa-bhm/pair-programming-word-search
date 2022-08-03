f5e83daabd990e833e0226cac5c4add924067d0f
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-03 23:49:17 +0000

    Pair programin Colton & Mustafa

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
+/// Pair programing Colton & Mustafa 
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    for (let c = 0; c < letters[0].length; c++){
+        let verticalJoin = "";
+        for (let r = 0; r < letters.length; r++){
+            verticalJoin += letters[r][c];
+            if (verticalJoin.includes(word)) return true;
+        }
     }
+  
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+  
+    return false;
 }
+
+
 
 module.exports = wordSearch
